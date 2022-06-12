55ed925bef866632baed831a1a5a6050e5facc4b
Iampelumi <107362587+Iampelumi@users.noreply.github.com>
2022-06-12 17:52:16 +0100

    Add files via upload

---
+++ First assignment.R
@@ -0,0 +1,56 @@
+#1 create five (5) variables withn the values 30, GOOD, 100, GREEN, ans NIGERIA
+#2 name the variables with: number, status, century, colour, and counytry respectively
+
+number<- 30
+status<- 'GOOD'
+century<- 100
+colour<- 'GREEN'
+country<- "NIGERIA"
+#3 types of data
+#30 - number data
+#status - character data
+#century - number data
+#colour - character data
+#country - character data
+
+#4 create five(5) numerical variables with the value 50, 100, 150, 200 and 250
+#5 assign a variable name a-e for the five(5) variables respectively
+#6 perform the following operations on the above variable: addition, substraction , multiplication and division)
+#7 perfomr the following operation between variable A and B, (<, >, ==, %/%, %%, !=)
+
+a<- 50
+b<- 100
+c<- 150
+d<- 200
+e<- 250
+
+add<- a + b
+add
+
+add<- a + b + c 
+add
+ sub<- e - b 
+sub 
+
+mult<- b * c
+mult
+
+divide<- c / a
+divide
+
+a<b
+a>b
+a==b
+a%/%b
+a%%b
+a!=b
+
+#8 create three(3) character variables: is a country, in Africa, Nigeria
+#name the country as: where, continent, country
+#write a code thatb return "Nigeria is a country in Africa
+
+where<- "is a country"
+continent<- "in Africa"
+country<- "Nigeria"
+
+paste("Nigeria", "is a counttry", "in Africa")
